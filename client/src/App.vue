5bcbc96a8d23a189cbf6ba493649a506da1dc317
surajrkn <surajmanikuttan@gmail.com>
2020-08-02 00:13:44 +0530

    ui changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'App'
+}
+</script>
 <style lang="scss">
+@import  './assets/css/style.scss';
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav">
+        <div class="logo">Logo</div>
+        <ul>
+            <li>Login</li>
+            <li>Register</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Nav'
+
+}
+</script>
+<style lang="scss" scoped>
+  .nav {
+    max-width: 1200px;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto
+  }
+  ul{
+    margin: 0;
+    padding: 0;
+
+    li {
+      display: inline-block;
+      padding: 15px;
+    }
+  }
+</style>
